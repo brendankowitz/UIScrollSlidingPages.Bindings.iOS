ee7e42309ad07ef13e1de583a252b0be5ede2d60
Shannon Lewis <slewis74@gmail.com>
2015-02-02 17:29:17 +1000

    Updated to the Unified API.

--- SlidingPages.Bindings/libUIScrollSlidingPagesBindingsSDK.linkwith.cs
+++ SlidingPages.Bindings/libUIScrollSlidingPagesBindingsSDK.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
-using MonoTouch.ObjCRuntime;
+using ObjCRuntime;
 
-[assembly: LinkWith ("libUIScrollSlidingPagesBindingsSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, ForceLoad = true, Frameworks = "Foundation UIKit")]
+[assembly: LinkWith ("libUIScrollSlidingPagesBindingsSDK.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Arm64 | LinkTarget.Simulator64, ForceLoad = true, Frameworks = "Foundation UIKit")]
